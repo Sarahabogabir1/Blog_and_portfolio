92faa6613e84f314add712a7f716e5d92c7da1d1
Sarah Abogabir <sarahabogabir@gmail.com>
2017-01-21 18:27:13 -0500

    Sets up projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # We don't need to add resources for welcome page since we only will need one of the resources
+  resources :posts
+  resources :projects
   get 'welcome/index'
     root 'welcome#index' #settig the action to the welcome controller
-  resources :posts
 end
